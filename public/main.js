aa146a90eac46b6a9c9866022bc7053f2a4afc9c
aleksei24 <lexa24032@gmail.com>
2021-07-20 21:05:38 +0300

    routes are stored in a separate file

--- public/main.js
+++ public/main.js
@@ -1,25 +1,6 @@
 window.onload = () => {
     let root = document.getElementById('app');
 
-    let Router = function (name, routes) {
-        return { name, routes };
-    };
-
-    let routerInstance = new Router('routerInstance', [
-        {
-            path: '/',
-            name: 'Root',
-        },
-        {
-            path: '/about',
-            name: 'About',
-        },
-        {
-            path: '/contact',
-            name: 'Contact',
-        },
-    ]);
-
     let currentPath = window.location.pathname;
 
     if (currentPath === '/') {
---
+++ public/routes.js
@@ -0,0 +1,20 @@
+let Router = function (name, routes) {
+    return { name, routes };
+};
+
+let routerInstance = new Router('routerInstance', [
+    {
+        path: '/',
+        name: 'Root',
+    },
+    {
+        path: '/about',
+        name: 'About',
+    },
+    {
+        path: '/contact',
+        name: 'Contact',
+    },
+]);
+
+export default routerInstance;
